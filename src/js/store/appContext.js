97d11711eb2418b3da8a492eeb52ea06b655584e
matiasmunozs <chmunozs@gmail.com>
2022-07-11 21:11:56 -0400

    arreglo en favoritos

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -35,7 +35,6 @@ const injectContext = PassedComponent => {
 			state.actions.fetchPeople();
 			state.actions.fetchVehicles();
 			state.actions.fetchPlanets();
-			state.actions.addFavorite();
 
 		}, []);
 
