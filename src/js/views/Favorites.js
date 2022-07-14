70314874503e9844f22c6ef72453a088652b64bc
matiasmunozs <chmunozs@gmail.com>
2022-07-13 21:44:00 -0400

    limpieza de codigo

--- src/js/views/Favorites.js
+++ src/js/views/Favorites.js
@@ -6,30 +6,6 @@ const Favorites = () => {
     const { store, actions } = useContext(Context)
 
     return (
-        // <>
-        //     <h1>Favorites</h1>
-
-        //       {store.favorites.map((favorites, id) => {
-
-        //         return (
-
-        //             <div key={id} className="card" style={{ width: '18rem' }}>
-        //                 <div className="card-body">
-        //                     <h5 className="card-title">Name: {favorites}</h5>
-        //                     <button onClick={() => actions.deleteFavorite(id)}>
-        // 							{" "}
-        // 							X {" "}
-        // 						</button>
-
-
-        //                 </div>
-        //             </div>
-
-        //         )
-        //     }
-        //     )}
-
-
 
         <>
             <h1>Favorites</h1>
